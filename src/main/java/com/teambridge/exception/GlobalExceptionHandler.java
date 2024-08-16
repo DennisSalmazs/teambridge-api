1ead1cf5df6182385b93b2e4e96cb5e28c16525a
DennisSalmazs <skrn.sdet@gmail.com>
2024-08-16 15:03:29 +0100

    global exception handler

--- src/main/java/com/teambridge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/teambridge/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.teambridge.exception;
 
-import com.teambridge.dto.ResponseWrapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,5 +19,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ExceptionWrapper(exception.getMessage(), HttpStatus.NOT_FOUND.value()));
+    }
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<ExceptionWrapper> userAlreadyExistException(UserAlreadyExistException exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ExceptionWrapper(exception.getMessage(), HttpStatus.CONFLICT.value()));
+    }
+
+    @ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+    public ResponseEntity<ExceptionWrapper> genericException(Throwable exception) {
+        exception.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionWrapper("An error occurred. Try again!", HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 }
